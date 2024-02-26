e2e0328e8c2cd8101b38fecaaf385b6f6bfbb6aa
Rafael <rafaelnonato79@gmail.com>
2024-02-26 01:05:12 -0300

    adicionado nova pagina

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AD72115B9CDAE873B2234BD0908CEF7B45A2D9AD9C66C699239B46E582F70A8B/scale?width=1200&aspectRatio=1.78&format=webp",
+    "title":"NOVO HOMEM DE FERRO É REVELADO",
+    "description":"saiu novo filme do homem de ferro"
+  },
+  {
+    "id":"2",
+    "photoCover":"aaa",
+    "title":"saiu novo filme",
+    "description":"saiu novo filme do homem de ferro"
+  }
+]
