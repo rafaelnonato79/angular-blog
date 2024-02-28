171521a4165ec857939210a5f56b8c8a57d311e7
Rafael Nonato <rafaelnonato79@gmail.com>
2024-02-27 22:10:10 -0300

    Adicionado conteudo as paginas

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,13 +2,26 @@ export const dataFake = [
   {
     "id":"1",
     "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AD72115B9CDAE873B2234BD0908CEF7B45A2D9AD9C66C699239B46E582F70A8B/scale?width=1200&aspectRatio=1.78&format=webp",
-    "title":"NOVO HOMEM DE FERRO É REVELADO",
-    "description":"saiu novo filme do homem de ferro"
+    "title":"RETORNO DE ROBERT DOWNEY JR. COMO HOMEM DE FERRO?",
+    "description":"Fonte confiável revela o filme da Marvel em que Robert Downey Jr. retornará como Homem de Ferro  O ator Robert Downey Jr. está de volta ao Universo Cinematográfico da Marvel (MCU) como Homem de Ferro. A informação foi revelada por uma fonte confiável, que já acertou outras previsões sobre o MCU. O retorno do ator como Tony Stark foi confirmado por Daniel Richtman, insider que já acertou outras previsões sobre o MCU. Richtman não deu mais detalhes sobre o retorno de Downey Jr. como Homem de Ferro, mas a notícia já é suficiente para deixar os fãs animados. O ator se despediu do papel em Vingadores: Ultimato (2019), mas já havia manifestado interesse em retornar ao MCU. A Marvel Studios também já havia manifestado interesse em contar com o ator em futuros projeto"
   },
   {
     "id":"2",
-    "photoCover":"aaa",
-    "title":"saiu novo filme",
-    "description":"saiu novo filme do homem de ferro"
+    "photoCover":"https://manualdosgames.com/wp-content/uploads/2024/01/Avatar-A-Lenda-de-Aang-Netflix.jpg.webp",
+    "title":"Avatar: O Último Mestre do Ar” estreia na Netflix",
+    "description":"A adaptação live-action “Avatar: O Último Mestre do Ar” chega à Netflix nesta quinta-feira (22). A série conta com oito episódios de cerca de uma hora. Na trama, as quatro nações – Água, Terra, Fogo e Ar – vivia em harmonia, com o Avatar, mestre da todos os quatro elementos, mantendo a paz entre eles, mas tudo mudou quando o Fogo atacou e exterminou os Nômades do Ar, com o objetivo de conquistar o mundo. Com a atual encarnação do Avatar ainda por surgir, o mundo perdeu a esperança. Mas a esperança surge quando Aang, um jovem nômade do Ar – e o último de sua espécie – desperta para ocupar seu lugar de direito como o próximo avatar."
+  },
+  {
+    "id":"3",
+    "photoCover":"https://t.ctcdn.com.br/0IJKOhgz89JFcWifaDK7t0mo7D0=/4096x2304/smart/i747410.jpeg",
+    "title":"Avatar: O Último Mestre do Ar” estreia na Netflix",
+    "description":"Filme que será lançado em Junho de 2024 tem como sinpse: Spider-Man: Beyond The Spider-Verse até o momento intitulado como Homem-Aranha: Além do Aranhaverso, é a futura animação da Sony que fechará a trilogia das aventuras de Miles Morales (Shameik Moore). A história acompanha os desfechos  de Homem-Aranha: Através do Aranhaverso, após Miles ir parar em outro dimensão e descobrir a verdade sobre sua família. "
+  },
+  {
+    "id":"4",
+    "photoCover":"https://uploads.jovemnerd.com.br/wp-content/uploads/2023/02/the_flash_filme_tudo_sobre__3b22pj1-1210x544.jpg",
+    "title":"Qual é a história de The Flash?",
+    "description":"Baseada na HQ Ponto de Ignição, lançada em 2011, a trama de The Flash traz Barry Allen viajando ao passado para evitar a morte da mãe. O problema é que nenhuma boa ação fica sem punição e as ações do herói causam tumultos no multiverso. Ele acaba caindo em uma outra realidade onde meta-humanos como o Superman nunca existiram e encontra uma versão alternativa de si próprio. Os “Flashes” então devem consertar a linha do tempo com uma ajudinha providencial da Supergirl (Calle) e de um certo Homem-Morcego (Keaton)."
   }
+
 ]
